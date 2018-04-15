b1370bec88655e5885275e43f1e2c9eee77ce0d2
ochoa.kayla@yahoo.com <ochoa.kayla@yahoo.com>
2018-04-15 19:35:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function prependKitten(name) {
   return [name, ...kittens];
 }
 
-function removeLastKitten(kittens) {
+function removeLastKitten() {
   return kittens.slice(0, kittens.length - 1);
 }
 
