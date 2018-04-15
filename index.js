15d223643aea3a519c6cd2ea414a986cb5d6f278
ochoa.kayla@yahoo.com <ochoa.kayla@yahoo.com>
2018-04-15 19:37:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,6 +36,6 @@ function removeLastKitten() {
   return kittens.slice(0, kittens.length - 1);
 }
 
-function removeFirstKitten(kittens) {
+function removeFirstKitten() {
   return kittens.slice(1);
 }
